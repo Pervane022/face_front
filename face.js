1ee63423dd0994c21c1750bafaa89c7bddcdfe51
Pervane-code <pervaneuluxanova98@gmail.com>
2020-11-14 08:10:30 +0300

    first_commit

---
+++ face.js
@@ -0,0 +1,16 @@
+function Process(){
+    var x=document.getElementById('set_dp');
+    if(x.style.display === "none"){
+        x.style.display="block";
+    } else{
+        x.style.display="none";
+    }
+}
+function Processone(){
+    var x=document.getElementById('src_dp');
+    if(x.style.display === "none"){
+        x.style.display="block";
+    } else{
+        x.style.display="none";
+    }
+}
